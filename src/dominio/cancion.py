a5a6b5d0d5e2f1c1fcf245746a43e8c1581b4345
celesteContardi <mariacelestecontardid@gmail.com>
2026-09-20 17:15:37 -0300

    add new file edit

--- src/dominio/cancion.py
+++ src/dominio/cancion.py
@@ -10,32 +10,32 @@ class Cancion:
     self._anio = int(anio)
     self._duracion_seg = int(duracion_seg)
      
-def id (self) -> int:
-  return self._id 
+  def id (self) -> int:
+   return self._id 
 
-def titulo (self) -> str:
-  return self._titulo
+  def titulo (self) -> str:
+   return self._titulo
 
-def artista (self) -> str:
-  return self._artista
+  def artista (self) -> str:
+   return self._artista
 
-def album (self) -> str:
-  return self._album
+  def album (self) -> str:
+   return self._album
 
-def genero (self) -> str:
-  return self._genero
+  def genero (self) -> str:
+   return self._genero
 
-def anio (self) -> int:
-  return self._anio
+  def anio (self) -> int:
+   return self._anio
 
-def duracion_seg (self) -> int:
-  return self._duracion_seg
+  def duracion_seg (self) -> int:
+   return self._duracion_seg
 
-def duracion_formateada (self) -> str:
-  minutos = self._duracion_seg // 60
-  segundos = self._duracion_seg % 60
-  return f"{minutos:0d}:{segundos:0d}"
+  def duracion_formateada (self) -> str:
+   minutos = self._duracion_seg // 60
+   segundos = self._duracion_seg % 60
+   return f"{minutos:0d}:{segundos:0d}"
 
-def _str_ (self):
-  return f:"""[{self._id}]" {self._titulo} - {self._artista} \n
-  Álbum: {self._album} | Año de lanzamiento: {self._anio} | Género: {self._genero} | Duración: {self.duracion_formateada}"""
+  def __str__ (self):
+   return f:"""[{self._id}]" {self._titulo} - {self._artista} \n
+   Álbum: {self._album} | Año de lanzamiento: {self._anio} | Género: {self._genero} | Duración: {self.duracion_formateada()}"""
